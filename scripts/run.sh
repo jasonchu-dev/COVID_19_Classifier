c47471ca9b6ecea2f11d7cbf06b24be912421c08
jasonchu-dev <74292454+jasonchu-dev@users.noreply.github.com>
2023-11-23 20:09:31 -0800

    whoop

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,7 +4,7 @@ IMAGE_NAME="covid-19-classifier-img"
 CONTAINER_NAME="covid-19-classifier"
 
 docker build -t $IMAGE_NAME --no-cache .
-docker run --gpus all --name $CONTAINER_NAME -p 8888:8888 $IMAGE_NAME
+docker run -dit --gpus all --name $CONTAINER_NAME -p 8888:8888 $IMAGE_NAME
 
 while [ "$(docker inspect -f '{{.State.Running}}' "$CONTAINER_NAME")" != "true" ]; do
     sleep 1
